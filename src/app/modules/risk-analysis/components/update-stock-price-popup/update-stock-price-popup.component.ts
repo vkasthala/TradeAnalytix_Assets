d5692d1120920fe9e9f68d5cc7ef3ddee0cd064e
mallikarjunb <bmallikarjun9@gmail.com>
2021-01-18 09:19:15 +0530

    Added update stock price popup in risk analysis

---
+++ src/app/modules/risk-analysis/components/update-stock-price-popup/update-stock-price-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-stock-price-popup',
+  templateUrl: './update-stock-price-popup.component.html',
+  styleUrls: ['./update-stock-price-popup.component.scss']
+})
+export class UpdateStockPricePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/risk-analysis/risk-analysis.module.ts
+++ src/app/modules/risk-analysis/risk-analysis.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UpdateStockPricePopupComponent } from './components/update-stock-price-popup/update-stock-price-popup.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [UpdateStockPricePopupComponent],
   imports: [
     CommonModule
   ]
