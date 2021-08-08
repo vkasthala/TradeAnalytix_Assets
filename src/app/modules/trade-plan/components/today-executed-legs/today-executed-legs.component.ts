0d3aa6d667d20796cd5c0036c0c0bf4efcfd4713
Ramaswamy <swamyrama71@gmail.com>
2021-08-08 10:42:51 +0530

    Trade Plan Mobile Version - Issues are fixed

--- src/app/modules/trade-plan/components/today-executed-legs/today-executed-legs.component.ts
+++ src/app/modules/trade-plan/components/today-executed-legs/today-executed-legs.component.ts
@@ -12,7 +12,8 @@ export class TodayExecutedLegsComponent implements OnInit {
   todayExecutedGridColumns: string[] = ['strategyUid', 'symbol', 'action', 'quantity', 'strike', 'expiryDate', 'optionType', 'entryPrice', 'exitPrice'];
 
   todayExecutedLegsDatasource: TodayExecutedLeg[] = [];
-
+  todayexecutedGridData: TodayExecutedLeg[] = [];
+  public hideRuleContent: boolean[] = [];
   @Input('day') day: string;
 
   constructor(private tradePlanService: TradePlansService) { }
@@ -26,7 +27,13 @@ export class TodayExecutedLegsComponent implements OnInit {
     this.tradePlanService.getTodayExecutedLegs(this.day).subscribe(result => {
       console.log('today legs::', result);
       this.todayExecutedLegsDatasource = result;
+      this.todayexecutedGridData = result;
     });
+  }
+
+  Collaps(index: number) {
+    // this.expandedIndex[index] = !this.expandedIndex[index];
+    this.hideRuleContent[index] = !this.hideRuleContent[index];
   }
 
 }
