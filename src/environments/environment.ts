18f2c24b02b585cae4289a522df748df36abe7cd
mallikarjunb <bmallikarjun9@gmail.com>
2021-08-14 21:20:42 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://ec2-3-137-151-176.us-east-2.compute.amazonaws.com:8080',
   redirectUri: 'http://localhost:4200/oauth-redirect'
 };
 
