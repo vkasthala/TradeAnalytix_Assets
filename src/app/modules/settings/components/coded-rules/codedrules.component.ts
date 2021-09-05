fc9f1cde2ae38520b842341980b1d61e17ca64bf
mallikarjunb <bmallikarjun9@gmail.com>
2021-09-05 15:26:56 +0530

    Modified coded rules UI integration as per new design

--- src/app/modules/settings/components/coded-rules/codedrules.component.ts
+++ src/app/modules/settings/components/coded-rules/codedrules.component.ts
@@ -1,78 +1,34 @@
-import { stringify } from '@angular/compiler/src/util';
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { of, Subject } from 'rxjs';
-import { ConfirmDialogComponent } from 'src/app/modules/shared/components/modals/confirm-dialog/confirm-dialog.component';
-import { EditableGridComponent } from 'src/app/modules/shared/components/widgets/editable-grid/editable-grid.component';
 import { EditableGridColumn } from 'src/app/modules/shared/models/common/editable-grid-column.model';
-import { BockerageCommission } from '../../models/brockerage-commission.model';
 import { UserCodedRule } from '../../models/user-coded-rule.model';
 import { CodedRuleService } from '../../services/coded-rule.service';
-import { DataSetupService } from '../../services/data-setup.service';
 
 @Component({
   selector: 'app-codedrules',
   templateUrl: './codedrules.component.html',
   styleUrls: ['./codedrules.component.scss']
 })
-export class CodedRulesComponent<T> implements OnInit {
+export class CodedRulesComponent implements OnInit {
 
-  @ViewChild('codedRules', { static: false }) protected codedRules: EditableGridComponent<UserCodedRule>;
-  
-  //paramNameCol: EditableGridColumn;
   ruleOperatorCol: EditableGridColumn;
   valueCol: EditableGridColumn;
 
-  columnConfigs: EditableGridColumn[] = [{"id":"1000", "name":"Receive an alert while adding or editing a trade when the stock amount is greater than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"500", "name":"Receive an alert while adding or editing a trade when the option amount is greater than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"10000", "name":"Receive an alert while adding or editing a trade when the total amount of a trade is greater than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"50000", "name":"Receive an alert while adding or editing a trade when maximum risk of a trade is greater than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"50000", "name":"Receive an alert while adding or editing a trade when maximum profit potential of a trade is less than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"", "name":"Receive an alert wh,,en a trade is being averaged", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"50000", "name":"Receive an alert at the end of the day when maximum risk of the account greater than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"50000", "name":"Receive an alert at the end of the day when maximum profit potential of the account is less than", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"", "name":"Receive an alert when a trade plan is not created for the day", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"20", "name":"Receive an alert at the end of the day when daily trade count exceeds", "type":"text", "values":[],"placeholder":"", "visible":false},
-  {"id":"120", "name":"Receive an alert at the end of the day when weekly trade count exceeds", "type":"text", "values":[],"placeholder":"", "visible":false}
-];
+  userCodedRules: UserCodedRule[];
 
-  selectedModel: T;
-  edit: boolean = false;
-  addItemSubject: Subject<T>;
-  editItemSubject: Subject<T>;
-  deleteItemSubject: Subject<T>;
-  comboChangeSubject: Subject<string>;
-
-  constructor(private codedRuleService: CodedRuleService, private cdr: ChangeDetectorRef, private _dialog: MatDialog, private toastr: ToastrService) { }
-  step = 0;
+  constructor(private codedRuleService: CodedRuleService, private toastr: ToastrService) { }
 
   ngOnInit() {
 
   }
 
-  setStep(index: number) {
-    this.step = index;
-  }
-
   ngAfterViewInit() {
-    //this.loadCodedRuleOptions();
-    //this.initCodedRulesGrid();
+    this.loadCodedRulesData();
   }
-
-  /*loadCodedRuleOptions() {
-    this.codedRuleService.getCodedRules().subscribe(result => {
-      let arr = [];
-      result.forEach(rule => {
-        arr.push([rule.id, rule.label]);
-      });
-      this.paramNameCol.values = arr;
-    });
-  }*/
 
   loadCodedRulesData() {
     this.codedRuleService.getUserCodedRules().subscribe(result => {
-      this.codedRules.dataSource = result;
+      this.userCodedRules = result;
     });
   }
 
@@ -166,28 +122,6 @@ export class CodedRulesComponent<T> implements OnInit {
     this.codedRules.comboChangeSubject = comboChangeSubject;
   }*/
 
-  lodRuleOperators(ruleId: string) {
-    this.codedRuleService.getRuleOperators(ruleId).subscribe(result => {
-      let arr = [];
-      result.forEach(rule => {
-        arr.push([rule.id, rule.name]);
-      });
-      this.ruleOperatorCol.values = arr;
-    });
-  }
-
-  getDeleteDialog() {
-    const dialogRef = this._dialog.open(ConfirmDialogComponent, {
-      width: 'auto',
-      height: 'auto',
-      data: { 'message': 'Are you sure you want to delete this entry?' }
-    });
-    return dialogRef;
-  }
-
-  showDeleteErrorMessage() {
-    this.toastr.error('Failed to delete entry. Please check if this has assigned to any trade strategy.', '');
-  }
 
   showErrorMessageDialog(msg: string) {
     this.toastr.error(msg, '');
@@ -197,22 +131,39 @@ export class CodedRulesComponent<T> implements OnInit {
     this.toastr.success(msg);
   }
 
-  onRowEdit(element) {
+  onRowEdit(element: UserCodedRule) {
     console.log(element);
-    this.selectedModel = element;
-    element.visible = true;
+    element.editing = true;
+    element.tempVal = element.val ? element.val : element.tempVal;
   }
-  onItemEdit(element) {
-    for (let ind = 0; ind < this.columnConfigs.length; ind++) {
-      let ele = document.getElementById(this.columnConfigs[ind].id);
-      if (ele) {
-        this.selectedModel[this.columnConfigs[ind].id] = ele['value'];
-      }
+
+  onItemEdit(element: UserCodedRule) {
+    if (!element.tempVal || element.tempVal.length === 0) {
+      return;
     }
-    element.visible = false;
+    element.val = element.tempVal;
+    element.editing = false;
+    if (!element.id || element.id === 0) {
+      this.codedRuleService.createCodedRule(element).subscribe(data => {
+        this.showSuccessMessage('Successfully added the coded rule');
+        this.loadCodedRulesData();
+      }, err => {
+        console.log('error in creating coded rule: ', element)
+        this.showErrorMessageDialog('Error! failed to add coded rule');
+      });
+    } else {
+      this.codedRuleService.updateCodedRule(element).subscribe(data => {
+        this.showSuccessMessage('Successfully updated the coded rule');
+        this.loadCodedRulesData();
+      }, err => {
+        console.log('error in editing coded rule: ', element)
+        this.showErrorMessageDialog('Error! failed to edit coded rule');
+      });
+    }
   }
-  onCancel(element) {
-    element.visible = false;
+
+  onCancel(element: UserCodedRule) {
+    element.editing = false;
   }
 
 }
