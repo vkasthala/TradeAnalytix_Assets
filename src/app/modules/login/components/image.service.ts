e17e0bca51c911f57d1c36e637bb27581e27feb6
mallikarjunb <bmallikarjun9@gmail.com>
2023-01-06 22:10:59 +0530

    svg logos updated from landing page.

--- src/app/modules/login/components/image.service.ts
+++ src/app/modules/login/components/image.service.ts
@@ -12,44 +12,84 @@ export class ImageService {
 data:any= 
     [
         {
-          "image": "../../../../assets/images/slideshow/e-trade.png",
-          "thumbImage": "../../../../assets/images/slideshow/e-trade.png",
+          "image": "../../../../assets/images/slideshow/img1.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img1.svg",
           "order":1
         },
         {
-          "image": "../../../../assets/images/slideshow/img2.png",
-          "thumbImage": "../../../../assets/images/slideshow/img2.png",
+          "image": "../../../../assets/images/slideshow/img2.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img2.svg",
           "order":2
         },
         {
-          "image": "../../../../assets/images/slideshow/img3.png",
-          "thumbImage": "../../../../assets/images/slideshow/img3.png",
+          "image": "../../../../assets/images/slideshow/img3.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img3.svg ",
           "order":3
         },
         {
-          "image": "../../../../assets/images/slideshow/img4.png",
-          "thumbImage": "../../../../assets/images/slideshow/img4.png",
+          "image": "../../../../assets/images/slideshow/img4.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img4.svg",
           "order":4
         },
         {
-          "image": "../../../../assets/images/slideshow/img5.png",
-          "thumbImage": "../../../../assets/images/slideshow/img5.png",
+          "image": "../../../../assets/images/slideshow/img5.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img5.svg",
           "order":5
         },
         {
-          "image": "../../../../assets/images/slideshow/img6.png",
-          "thumbImage": "../../../../assets/images/slideshow/img6.png",
+          "image": "../../../../assets/images/slideshow/img6.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img6.svg",
           "order":6
         },
         {
-          "image": "../../../../assets/images/slideshow/img7.png",
-          "thumbImage": "../../../../assets/images/slideshow/img7.png",
+          "image": "../../../../assets/images/slideshow/img7.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img7.svg",
           "order":7
         },
         {
-          "image": "../../../../assets/images/slideshow/img8.png",
-          "thumbImage": "../../../../assets/images/slideshow/img8.png",
+          "image": "../../../../assets/images/slideshow/img8.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img8.svg",
           "order":8
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img1.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img1.svg",
+          "order":9
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img2.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img2.svg",
+          "order":10
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img3.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img3.svg ",
+          "order":11
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img4.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img4.svg",
+          "order":12
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img5.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img5.svg",
+          "order":13
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img6.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img6.svg",
+          "order":14
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img7.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img7.svg",
+          "order":15
+        },
+        {
+          "image": "../../../../assets/images/slideshow/img8.svg",
+          "thumbImage": "../../../../assets/images/slideshow/img8.svg",
+          "order":16
         },
       ] ;
   getImages() {
