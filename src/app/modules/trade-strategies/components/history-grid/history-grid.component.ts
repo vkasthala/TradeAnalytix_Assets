a14ad8907e041824952d5e2f4e3deaee54945e82
Venkat <venkat.kasthala@gmail.com>
2024-11-24 15:16:43 -0600

    Removed Identifier column from Open and Closed Positons grid

--- src/app/modules/trade-strategies/components/history-grid/history-grid.component.ts
+++ src/app/modules/trade-strategies/components/history-grid/history-grid.component.ts
@@ -31,7 +31,9 @@ export class HistoryGrid implements AfterViewInit, OnInit {
 
   protected Loader = false;
   expandIndex: any;
-  displayedColumns = ['action', 'stockName', 'id', 'openDate', 'closeDate', 'strategy', 'direction', 'journaled', 'netR', 'maxLoss', 'return', 'tags'];
+  // displayedColumns = ['action', 'stockName', 'id', 'openDate', 'closeDate', 'strategy', 'direction', 'journaled', 'netR', 'maxLoss', 'return', 'tags'];
+  // Remove id to remove identifier column from the open strategies grid
+  displayedColumns = ['action', 'stockName', 'openDate', 'closeDate', 'strategy', 'direction', 'journaled', 'netR', 'maxLoss', 'return', 'tags'];
   pageSize: number = 20
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
