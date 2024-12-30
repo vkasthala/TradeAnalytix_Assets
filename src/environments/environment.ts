39f41a0c1b8e8398d2e4b52535fe3f5857b761f0
arunn <arun.988@gmail.com>
2024-12-30 18:25:26 +0530

    updated snaptrade url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -26,7 +26,7 @@ export const environment = {
   // redirectUri: 'http://localhost:4200/oauth-redirect',
   // tradingServiceUri: 'http://localhost:8081/tradingservice',
   // websocketUrl: 'http://localhost:5001',
-  snaptradeurl:'http://localhost:8081',
+  snaptradeurl:'https://trading.cuetrade.io',
 
 
 
